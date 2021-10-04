0ee9b59129f944c3b424088d45503915ba5feec3
Morris Zuniga <mzuni04@my.centennialcollege.ca>
2021-10-03 21:49:58 -0400

    servicespage

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('', (req, res) => {
+  res.render('servicespage', { layout:'./partials/servicespage'})
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

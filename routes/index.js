c4dc7300b89e41f64868c1616eb0f4ecfb53892c
Morris Zuniga <mzuni04@my.centennialcollege.ca>
2021-10-04 22:53:43 -0400

    edit9

--- routes/index.js
+++ routes/index.js
@@ -8,30 +8,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'main' });
-});
 
-/* GET About Me page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
-});
-
-/* GET Projects page.*/ 
-router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
-});
-
-/* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
-});
-
-/* GET Contact Me page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
-});
 
 
 

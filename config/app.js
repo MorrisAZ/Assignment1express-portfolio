c2cdd804a580fff2a46e1bc56a097d7d0850dc46
Morris Zuniga <mzuni04@my.centennialcollege.ca>
2021-10-17 14:12:28 -0400

    update files

--- config/app.js
+++ config/app.js
@@ -36,15 +36,15 @@ mongoDB.once('open', ()=>{
 let  app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');// express -e
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './node_modules')));
 
 //set routes
 app.use('/', indexRouter);//home
